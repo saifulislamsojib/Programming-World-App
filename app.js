382a4beed3b9d6cc456aa5208d866f55ff873985
saifulislamsojib <saifulislam.sojib56@gmail.com>
2022-09-24 00:37:36 +0600

    update

--- app.js
+++ app.js
@@ -1,100 +1,100 @@
-// initialize aos
-AOS.init({
-    duration : 2000
-});
+const navbar = document.getElementById("navbar");
 
 // Scroll toggle top
-const navbar = document.getElementById('navbar');
-window.addEventListener('scroll', () => {
-    if (window.scrollY > 5) {
-        navbar.classList.add('bgc-primary', 'toggle-top', 'shadow');
-    }
-    else {
-        navbar.classList.remove('bgc-primary', 'toggle-top', 'shadow');
-    }
-})
+const handleNavbarScroll = () => {
+  if (window.scrollY > 5) {
+    navbar.classList.add("bgc-primary", "toggle-top", "shadow");
+  } else {
+    navbar.classList.remove("bgc-primary", "toggle-top", "shadow");
+  }
+};
 
-// Active Link
-const links =  [...navbar.querySelectorAll('a.nav-link')];
-let count = 0;
-links.forEach(link => {
-    if (location.hash === link.getAttribute('href')) {
-        link.classList.add('active');
-        count++;
+handleNavbarScroll();
+window.addEventListener("scroll", handleNavbarScroll);
+
+// Active Links
+const handleActiveScroll = () => {
+  let activeLink;
+  const links = [...navbar.querySelectorAll("a.nav-link")];
+  const sections = document.getElementsByClassName("section");
+  [...sections].forEach((section) => {
+    const link = links.find(
+      (link) => link.getAttribute("href") === `#${section.id}`
+    );
+    if (section.offsetTop <= window?.scrollY + 100) {
+      activeLink = link;
     }
-    else {
-        link.classList.remove('active');
-    }
-    link.addEventListener('click', () => {
-        link.classList.add('active');
-        const other = links.filter(item => item.getAttribute('href')!==link.getAttribute('href'));
-        other.forEach(item => {
-            item.classList.remove('active');
-        })
-    })
-})
-if (!count) {
-    links[0].classList.add('active');
-}
+  });
+
+  links.forEach((link) => link.classList.remove("active"));
+  activeLink?.classList.add("active");
+};
+handleActiveScroll();
+window.addEventListener("scroll", handleActiveScroll);
+
+// initialize aos
+AOS.init({
+  duration: 2000,
+});
 
 const options = {
-    // Optional parameters
-    loop: true,
-    spaceBetween: 20,
+  // Optional parameters
+  loop: true,
+  spaceBetween: 20,
 
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
 
-    // Keyboard Control
-    keyboard: {
-        enabled: true,
-        onlyInViewport: false,
-    },
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 
-    // Responsive breakpoints
-    breakpoints: {
-        480: {
-            slidesPerView: 1
-        },
-        640: {
-            slidesPerView: 2
-        },
-        992: {
-            slidesPerView: 3
-        }
-    }
-}
+  // Keyboard Control
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+
+  // Responsive breakpoints
+  breakpoints: {
+    480: {
+      slidesPerView: 1,
+    },
+    640: {
+      slidesPerView: 2,
+    },
+    992: {
+      slidesPerView: 3,
+    },
+  },
+};
 
 // Initialize Swiper's
-const swiper = new Swiper('.feedback-swiper', options);
-const swiper2 = new Swiper('.screenshots-swiper', {
-    ...options,
-    // Responsive breakpoints
-    breakpoints: {
-        320: {
-            slidesPerView: 1
-        },
-        480: {
-            slidesPerView: 2
-        },
-        640: {
-            slidesPerView: 3
-        },
-        992: {
-            slidesPerView: 4
-        }
-    }
+const swiper = new Swiper(".feedback-swiper", options);
+const swiper2 = new Swiper(".screenshots-swiper", {
+  ...options,
+  // Responsive breakpoints
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    480: {
+      slidesPerView: 2,
+    },
+    640: {
+      slidesPerView: 3,
+    },
+    992: {
+      slidesPerView: 4,
+    },
+  },
 });
 
 setInterval(() => {
-    swiper.slideNext();
-    swiper2.slideNext();
+  swiper.slideNext();
+  swiper2.slideNext();
 }, 1500);
